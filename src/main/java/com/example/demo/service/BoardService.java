f123dff250a28a04af1d78603623cf79b81444be
h9661 <guscksdn123@gmail.com>
2023-09-20 15:49:18 +0900

    BoardService 수정

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -13,27 +13,11 @@ import java.util.UUID;
 @Service
 public class BoardService {
     @Autowired
-    // todo: 이 함수 수정하기
     private BoardRepository boardRepository;
 
-
-//    public void writeBoard(Board board, MultipartFile file) throws Exception {
-//        // 파일이 존재하면 파일을 저장하고, 파일 이름과 경로를 board 객체에 저장한다.
-//        if(file != null && !file.isEmpty()) {
-//            String fileSavingPath = System.getProperty("user.dir") + "/files/";
-//            UUID uuid = UUID.randomUUID();
-//            String fileName = uuid.toString() + "_" + file.getOriginalFilename();
-//
-//            File dest = new File(fileSavingPath, fileName);
-//            file.transferTo(dest);
-//
-//            board.setFilename(fileName);
-//            System.out.println(fileName);
-//            board.setFilepath("/files/" + fileName);
-//        }
-//
-//        boardRepository.save(board);
-//    }
+    public Board saveBoard(Board board) {
+        return boardRepository.save(board);
+    }
 
     public List<Board> getBoardList() {
         return boardRepository.findAll();
