43d8c897f5dcaed92813f4fe9beac7dc6c478a0a
h9661 <guscksdn123@gmail.com>
2023-09-20 20:32:10 +0900

    파일 업로드 크기 제한 설정

---
+++ src/main/java/com/example/demo/configuration/FileUploadConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.configuration;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+@Configuration
+public class FileUploadConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(1024L));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(1024L));
+
+        return factory.createMultipartConfig();
+    }
+}
