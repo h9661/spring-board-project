cdc82d02e88c0309f4705fc253c3abebd9fcf81b
h9661 <guscksdn123@gmail.com>
2023-09-20 15:47:46 +0900

    DTO로 이름 변경

--- src/main/java/com/example/demo/DTO/ImageFilesDTO.java
+++ src/main/java/com/example/demo/DTO/ImageFilesDTO.java
@@ -7,6 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Data
-public class ImageFiles {
+public class ImageFilesDTO {
     private List<MultipartFile> imageFiles;
 }
