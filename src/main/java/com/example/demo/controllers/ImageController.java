437468df1a8b48c8a14719e38c43ed749037dfe3
h9661 <guscksdn123@gmail.com>
2023-09-20 20:32:23 +0900

    오타 수정

--- src/main/java/com/example/demo/controllers/ImageController.java
+++ src/main/java/com/example/demo/controllers/ImageController.java
@@ -5,10 +5,7 @@ import com.example.demo.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/image")
@@ -16,9 +13,9 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
-    @PostMapping("/{id]/delete")
+    @PostMapping("/{id}/delete")
     @ResponseBody
-    public ResponseEntity<Image> deleteImage(Integer id) {
+    public ResponseEntity<Image> deleteImage(@PathVariable Integer id) {
         Image deletedImage = imageService.deleteById(id);
 
         return ResponseEntity.ok(deletedImage);
