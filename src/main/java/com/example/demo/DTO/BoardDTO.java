ce8badfbd22ae4710b6d26b65f29d59d13a0cbe6
h9661 <guscksdn123@gmail.com>
2023-09-25 19:50:35 +0900

    [Refactor] 이미지 리스트 제거
    이미지 객체를 사용하지 않기에 이미지 리스트를 제거했습니다.

--- src/main/java/com/example/demo/DTO/BoardDTO.java
+++ src/main/java/com/example/demo/DTO/BoardDTO.java
@@ -2,13 +2,9 @@ package com.example.demo.DTO;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class BoardDTO {
     Integer id;
     String title;
     String content;
-
-    List<ImageDTO> images;
 }
--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -3,8 +3,6 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.List;
-
 @Entity
 @Data
 public class Board {
@@ -14,7 +12,4 @@ public class Board {
     private String title;
     private String content;
 
-    // 1:n 관계 with Image
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
-    private List<Image> images;
 }
