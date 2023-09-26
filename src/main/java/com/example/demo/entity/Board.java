a0462f9ab69015e4e6f153a84c023d1e14145d5c
h9661 <guscksdn123@gmail.com>
2023-09-26 11:56:59 +0900

    [Refactor] Board Entity 리팩토링
    ddl-auto를 update로 변경해서 더 상세하게 entity class를 작성했다.

--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,7 +11,13 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 200)
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String content;
 
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<Comment> comments;
 }
