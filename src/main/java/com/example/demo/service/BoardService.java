7783c764d0d6cd55b7b4f750e796efcadd59389d
h9661 <guscksdn123@gmail.com>
2023-09-27 15:51:36 +0900

    [Refactor] Paging 처리를 위해 리팩터링
    board list를 가져올 때 Paging처리하여 가져올 수 있게 service를 수정했습니다.

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -6,6 +6,9 @@ import com.example.demo.entity.Board;
 import com.example.demo.entity.Comment;
 import com.example.demo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -31,8 +34,9 @@ public class BoardService {
         return boardRepository.save(newBoard);
     }
 
-    public List<Board> getBoardList() {
-        return boardRepository.findAll();
+    public Page<Board> getBoardList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return boardRepository.findAll(pageable);
     }
 
     public Board getBoard(Integer id) {
