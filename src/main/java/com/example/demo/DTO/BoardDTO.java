c599a393f1d13ed17d44b380558eb0cb18d1ce68
h9661 <guscksdn123@gmail.com>
2023-09-27 12:02:30 +0900

    [Refactor] validation 추가
    validation을 위해 몇가지 정보를 추가해주었다.

--- src/main/java/com/example/demo/DTO/BoardDTO.java
+++ src/main/java/com/example/demo/DTO/BoardDTO.java
@@ -1,10 +1,15 @@
 package com.example.demo.DTO;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BoardDTO {
-    Integer id;
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @Size(max = 200)
     String title;
+
+    @NotEmpty(message = "내용을 입력해주세요.")
     String content;
 }
--- src/main/java/com/example/demo/DTO/CommentDTO.java
+++ src/main/java/com/example/demo/DTO/CommentDTO.java
@@ -2,11 +2,12 @@ package com.example.demo.DTO;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class CommentDTO {
-  Integer id;
+  @NotEmpty(message = "내용을 입력해주세요.")
   String content;
   BoardDTO boardDTO;
   LocalDateTime createdAt;
