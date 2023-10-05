157b43aabf2446844f0ea80c41d7197668b29753
h9661 <guscksdn123@gmail.com>
2023-10-05 17:46:00 +0900

    [Add] spring security config added
    1. authorize all of request for path /**
    2. disable csrf for path /h2-console.
    3. disable frameoptions for /h2-console
    4. formLogin
    5. logout

---
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // authorize requests for all paths
+            .csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console"))) // ignore CSRF for h2-console
+            .headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable())) // disable frameOptions
+            .formLogin((formLogin) -> formLogin.permitAll()) // permit all form login
+            .logout((logout) -> logout.permitAll()); // permit all logout
+            
+        ;
+        return http.build();
+    }
+}
