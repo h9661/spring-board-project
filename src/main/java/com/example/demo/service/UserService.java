c02cdaf2cd3ba09bf0c4b166f3806544a08b5dae
h9661 <guscksdn123@gmail.com>
2023-10-12 15:20:19 +0900

    [Feat] 유저 이름으로 찾는 서비스 함수 구현
    디비에 유저가 존재하면 User entity를 리턴하고 없다면 커스텀 예외를 던진다.

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.DTO.UserDTO;
 import com.example.demo.entity.User;
+import com.example.demo.exception.DataNotFoundException;
 import com.example.demo.repository.UserRepository;
 
 @Service
@@ -22,5 +25,15 @@ public class UserService {
     user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
     user.setEmail(userDTO.getEmail());
     return userRepository.save(user);
+  }
+
+  public User getUserByUsername(String username) {
+    Optional<User> userOptional = userRepository.findByUsername(username);
+
+    if (userOptional.isPresent()) {
+      return userOptional.get();
+    } else {
+      throw new DataNotFoundException("User not found");
+    }
   }
 }
